ae2e60ca55a8a485b9927441abf2a07251b5b466
abir <abirchelligue83@gmail.com>
2025-04-20 14:12:10 +0200

    feat: user profile

---
+++ app/src/main/java/com/example/formationstagefv/ProfileActivity.java
@@ -0,0 +1,101 @@
+package com.example.formationstagefv;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class ProfileActivity extends AppCompatActivity {
+    private EditText fullName ,email, phoneNumber, cin, address;
+    private Button btnEditProfile, btnLogOut ;
+     private FirebaseAuth firebaseAuth;
+     private FirebaseDatabase firebaseDatabase;
+     private DatabaseReference databaseReference;
+     private FirebaseUser loggedUser ;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_profile);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        fullName= findViewById(R.id.full_name_profile);
+        email= findViewById(R.id.email_profile);
+        phoneNumber= findViewById(R.id.phone_profile);
+        cin= findViewById(R.id.cin_profile);
+        address= findViewById(R.id.address_profile);
+        btnEditProfile= findViewById(R.id.btn_edit_profile);
+        btnLogOut= findViewById(R.id.btn_log_out);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        loggedUser = firebaseAuth.getCurrentUser();
+        databaseReference =firebaseDatabase.getReference().child("Users").child(""+loggedUser.getUid());
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+             String fullNameString = snapshot.child("fullName").getValue().toString();
+             String emailString= snapshot.child("email").getValue().toString();
+             String phoneNumberString = snapshot.child("phoneNumber").getValue().toString();
+             String cinString = snapshot.child("cin").getValue().toString();
+             String  addressString = snapshot.child("address").getValue().toString();
+
+             fullName.setText(fullNameString);
+             email.setText(emailString);
+             phoneNumber.setText(phoneNumberString);
+             cin.setText(cinString);
+             address.setText(addressString);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ProfileActivity.this, "Error" + error, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        btnLogOut.setOnClickListener(v ->{
+            SharedPreferences sharedPreferences = getSharedPreferences("checkBox", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("rememberMe", false);
+            editor.apply();
+            firebaseAuth.signOut();
+            startActivity(new Intent(this,SignInActivity.class));
+            Toast.makeText(this, "Log out successfully", Toast.LENGTH_SHORT).show();
+            finish();
+        });
+
+
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/formationstagefv/SignInActivity.java
+++ app/src/main/java/com/example/formationstagefv/SignInActivity.java
@@ -2,8 +2,11 @@ package com.example.formationstagefv;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -32,6 +35,7 @@ public class SignInActivity extends AppCompatActivity {
     private static final String REGEX_PASSWORD = "^.*(?=.{8,})(?=..*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$";
     private FirebaseAuth firebaseAuth;
     private ProgressDialog progressDialog;
+    private CheckBox rememberMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,9 +53,29 @@ public class SignInActivity extends AppCompatActivity {
         btnSignIn = findViewById(R.id.btn_sign_in);
         emailSignIn = findViewById(R.id.email_sign_in);
         passwordSignIn = findViewById(R.id.password_sign_in);
+        rememberMe = findViewById(R.id.remember_me);
 
         firebaseAuth = FirebaseAuth.getInstance();
         progressDialog = new ProgressDialog(this);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("checkBox", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        boolean rememberIsChecked = sharedPreferences.getBoolean("rememberMe",false);
+        if (rememberIsChecked){
+         startActivity( new Intent(this,ProfileActivity.class));
+        }
+
+        rememberMe.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (buttonView.isChecked()) {
+                editor.putBoolean("rememberMe", true);
+                editor.apply();
+            } else {
+                editor.putBoolean("rememberMe", false);
+                editor.apply();
+            }
+
+        });
 
 
         goToForgotPassword.setOnClickListener(v -> {
@@ -83,7 +107,7 @@ public class SignInActivity extends AppCompatActivity {
         FirebaseUser loggedUser = firebaseAuth.getCurrentUser();
         if (loggedUser != null) {
             if (loggedUser.isEmailVerified()) {
-                startActivity(new Intent(this, HomeActivity.class));
+                startActivity(new Intent(this,ProfileActivity.class));
                 finish();
             } else {
                 Toast.makeText(this, "Please verify your email ", Toast.LENGTH_SHORT).show();
